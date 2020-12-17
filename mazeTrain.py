130e770a03daaa29626d399a460530f38779364a
giladh <gdpstd@gmail.com>
2020-12-17 17:33:51 +0200

    Add mazeTrain in main folder to solve import bugs

---
+++ mazeTrain.py
@@ -0,0 +1,125 @@
+import gym
+import torch
+import json
+import os
+import yaml
+from tqdm import trange
+
+from mazelab import MazeEnv
+
+# Register env:
+gym.envs.register(
+    id="RandomShapeMaze-v0",
+    entry_point=MazeEnv,
+    max_episode_steps=200,
+    kwargs={"width": 50, "height": 50, "max_shapes": 50,
+            "max_size": 8, "allow_overlap": False, "shape": None}
+)
+
+# Add maml_rl to path:
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("../maml_rl"))))
+
+# import from maml_rl:
+import maml_rl.envs
+from maml_rl.metalearners import MAMLTRPO
+from maml_rl.baseline import LinearFeatureBaseline
+from maml_rl.samplers import MultiTaskSampler
+from maml_rl.utils.helpers import get_policy_for_env, get_input_size
+from maml_rl.utils.reinforcement_learning import get_returns
+
+def main(args):
+
+    # get configs:
+    with open(args.config, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    if args.output_folder is not None:
+        if not os.path.exists(args.output_folder):
+            os.makedirs(args.output_folder)
+        policy_filename = os.path.join(args.output_folder, 'policy.th')
+        config_filename = os.path.join(args.output_folder, 'config.json')
+
+        with open(config_filename, 'w') as f:
+            config.update(vars(args))
+            json.dump(config, f, indent=2)
+
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+
+    env = gym.make(config['env-name'], **config.get('env-kwargs', {}))
+    env.close()
+
+    # Policy
+    policy = get_policy_for_env(env,
+                                hidden_sizes=config['hidden-sizes'],
+                                nonlinearity=config['nonlinearity'])
+    policy.share_memory()
+
+    # Baseline
+    baseline = LinearFeatureBaseline(get_input_size(env))
+
+    # Sampler
+    sampler = MultiTaskSampler(config['env-name'],
+                               env_kwargs=config.get('env-kwargs', {}),
+                               batch_size=config['fast-batch-size'],
+                               policy=policy,
+                               baseline=baseline,
+                               env=env,
+                               seed=args.seed,
+                               num_workers=args.num_workers)
+
+    metalearner = MAMLTRPO(policy,
+                           fast_lr=config['fast-lr'],
+                           first_order=config['first-order'],
+                           device=args.device)
+
+    num_iterations = 0
+    for batch in trange(config['num-batches']):
+        tasks = sampler.sample_tasks(num_tasks=config['meta-batch-size'])
+        # >> need to change the futures to use an expert's demos
+        futures = sampler.sample_async(tasks,
+                                       num_steps=config['num-steps'],
+                                       fast_lr=config['fast-lr'],
+                                       gamma=config['gamma'],
+                                       gae_lambda=config['gae-lambda'],
+                                       device=args.device)
+        logs = metalearner.step(*futures,
+                                max_kl=config['max-kl'],
+                                cg_iters=config['cg-iters'],
+                                cg_damping=config['cg-damping'],
+                                ls_max_steps=config['ls-max-steps'],
+                                ls_backtrack_ratio=config['ls-backtrack-ratio'])
+
+        train_episodes, valid_episodes = sampler.sample_wait(futures)
+        num_iterations += sum(sum(episode.lengths) for episode in train_episodes[0])
+        num_iterations += sum(sum(episode.lengths) for episode in valid_episodes)
+        logs.update(tasks=tasks,
+                    num_iterations=num_iterations,
+                    train_returns=get_returns(train_episodes[0]),
+                    valid_returns=get_returns(valid_episodes))
+
+        # Save policy
+        if args.output_folder is not None:
+            with open(policy_filename, 'wb') as f:
+                torch.save(policy.state_dict(), f)
+
+
+if __name__ == '__main__':
+    torch.set_num_threads(1)
+
+    # creating an empty args object to mimic input arguments:
+    import argparse
+    parser = argparse.ArgumentParser(description='Reinforcement learning with '
+        'Model-Agnostic Meta-Learning (MAML) - Train')
+
+    # args for running on mazelab
+    args.config = "./runMaze/randomShapeMaze.yaml"
+    args.output_folder = "maml-randomShapeMaze"
+    args.seed = None
+    args.num_workers = 8
+    # args.device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    args.device = ('cpu')
+
+    main(args)
