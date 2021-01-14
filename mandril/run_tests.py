b72076f5426819ad253c412de11f909d1e933a30
giladh <gdpstd@gmail.com>
2021-01-14 20:03:51 +0200

    Test various expert based policies

--- mandril/run_tests.py
+++ mandril/run_tests.py
@@ -1,47 +1,49 @@
 from test import test
 
-#%% Test Maml 10000
-print(">> Maml:")
-config_path = "maml-banditk5n10/config_maml_10000.json"
-policy_path = "maml-banditk5n10/policy_maml_10000.th"
-output_folder = "maml-banditk5n10/results_maml_10000.npz"
-seed = 1
-meta_batch_size = 20
-num_batches = 10
-num_workers = 8
-device = "cpu"
-alg = "maml"
-test(
-    config_path,
-    output_folder,
-    policy_path,
-    seed=seed,
-    num_workers=num_workers,
-    num_batches=num_batches,
-    meta_batch_size=meta_batch_size,
-    device=device,
-    alg=alg
-)
+def test_mandril(model_folder, expert_type, expert_args):
+    config_path = "configs/maml/bandit/bandit-k5-n10.yaml"
+    seed = 1
+    num_workers = 8
+    meta_batch_size = 20
+    num_batches = 100
+    num_workers = 8
+    device = "cpu"
+    alg = "mandril"
 
-#%% Test Mandril 10000
-print(">> Mandril:")
-config_path = "maml-banditk5n10/config_mandril_10000.json"
-policy_path = "maml-banditk5n10/policy_mandril_10000.th"
-output_folder = "maml-banditk5n10/results_mandril_10000.npz"
-seed = 1
-meta_batch_size = 20
-num_batches = 10
-num_workers = 8
-device = "cpu"
-alg = "mandril"
-test(
-    config_path,
-    output_folder,
-    policy_path,
-    seed=seed,
-    num_workers=num_workers,
-    num_batches=num_batches,
-    meta_batch_size=meta_batch_size,
-    device=device,
-    alg=alg
-)
+    output_folder = model_folder + "config.json"
+    policy_path = model_folder + "policy.json"
+
+    test(
+        config_path,
+        output_folder,
+        policy_path,
+        seed=seed,
+        num_workers=num_workers,
+        num_batches=num_batches,
+        meta_batch_size=meta_batch_size,
+        device=device,
+        alg=alg,
+        expert_type=expert_type,
+        expert_args=expert_args,
+    )
+
+#%% Test Perfect
+print(">> Mandril with perfect expert:")
+model_folder = "banditk5n10/perfect"
+expert_type = "perfect"
+expert_args = {}
+test_mandril(model_folder, expert_type, expert_args)
+
+#%% Test random from best 2
+print(">> Mandril with random from best 2:")
+model_folder = "banditk5n10/rand_from_2_best"
+expert_type = "rand_from_k_best"
+expert_args = {"k": 2}
+test_mandril(model_folder, expert_type, expert_args)
+
+#%% Test random from best 3
+print(">> Mandril with random from best 3:")
+model_folder = "banditk5n10/rand_from_3_best"
+expert_type = "rand_from_k_best"
+expert_args = {"k": 3}
+test_mandril(model_folder, expert_type, expert_args)
