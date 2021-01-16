208dbdd20dab29b4416eaa534733086e6ef4846f
giladh <gdpstd@gmail.com>
2021-01-16 20:57:08 +0200

    Add rand_from_rand_best expert

--- mab/mabExpert.py
+++ mab/mabExpert.py
@@ -8,6 +8,7 @@ class MabExpert:
         self.experts = {
             "perfect": self.perfect,
             "rand_from_k_best": self.rand_from_k_best,
+            "rand_from_rand_best": self.rand_from_rand_best,
         }
 
     def get_actions(self, observations_tensor, envs_vec):
@@ -22,3 +23,7 @@ class MabExpert:
         N = arms.shape[0]
         rand_ind = torch.randint(0, self.values["k"], (N,1))
         return sorted_arm_inds[torch.arange(0,N)[:,None],rand_ind].squeeze()
+
+    def rand_from_rand_best(self):
+        self.values["k"] = int(torch.randint(0, self.values["max_k"], (1,))) + 1
+        return self.rand_from_k_best()
--- mandril/shell_train.sh
+++ mandril/shell_train.sh
@@ -1,8 +1,28 @@
+echo "---------------------------------------------"
+
+echo ">> Mandril with random from best random k=5:"
+python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_rand_best_5' --expert-type rand_from_rand_best --expert-args '{"max_k": 5}'
+
+echo ">> Mandril with random from best random k=3:"
+python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_rand_best_3' --expert-type rand_from_rand_best --expert-args '{"max_k": 3}'
+
+echo ">> Mandril with random from best random k=2:"
+python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_rand_best_2' --expert-type rand_from_rand_best --expert-args '{"max_k": 2}'
+
+echo ">> Mandril with random from best random k=4:"
+python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_rand_best_4' --expert-type rand_from_rand_best --expert-args '{"max_k": 4}'
+
+echo "---------------------------------------------"
+
 echo ">> Mandril with perfect expert:"
 python3 mandril/run_mandril.py --output-folder 'banditk5n10/perfect' --expert-type perfect --expert-args '{}'
+
+echo "---------------------------------------------"
 
 echo ">> Mandril with random from best 2:"
 python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_2_best' --expert-type rand_from_k_best --expert-args '{"k": 2}'
 
 echo ">> Mandril with random from best 3:"
 python3 mandril/run_mandril.py --output-folder 'banditk5n10/rand_from_3_best' --expert-type rand_from_k_best --expert-args '{"k": 3}'
+
+echo "---------------------------------------------"
