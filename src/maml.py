a9b5428c4ce1da9538a5bbf151d38b38b6bddb4a
giladh <gdpstd@gmail.com>
2021-02-16 00:08:47 +0200

    Finish working maml algorithm without graphs

--- src/maml.py
+++ src/maml.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 
-import gridworld as W
-import maxent as M
-import plot as P
-import trajectory as T
-import solver as S
-import optimizer as O
 import utils
 
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-def maml_iteration(batch_size, theta):
+def maml_iteration(batch_size, theta, size=5, p_slip=0):
     # set-up mdp
-    world, reward, terminal = utils.setup_mdp()
-
+    world, reward, terminal = utils.setup_mdp(size, p_slip)
+    # get expert trajectories
     trajectories, expert_policy = utils.generate_trajectories(
         world,
         reward,
-        terminal
+        terminal,
+        batch_size
     )
+    # optimize with maxent
     theta_maxent, reward_maxent = utils.maxent(
         world,
         terminal,
         trajectories
     )
+
+    # optimize with maxent - causal
     # theta_maxcausal, reward_maxcausal = utils.maxent_causal(
     #     world,
     #     terminal,
@@ -33,22 +31,28 @@ def maml_iteration(batch_size, theta):
     # )
     return theta_maxent, reward_maxent
 
-def maml(N=100, batch_size=20, meta_lr=0.1, theta=O.Constant(1.0)):
+def maml(N=100, batch_size=20, meta_lr=0.1, size=5, p_slip=0, theta=None):
     for ind in range(N):
         startTime = time.time()
-        phi, reward = maml_iteration(batch_size, theta)
-        theta = theta + meta_lr * (phi - theta)
+        phi, reward = maml_iteration(batch_size, theta, size, p_slip)
+        if theta is None:
+            theta = phi
+        else:
+            theta = theta + meta_lr * (phi - theta)
         executionTime = (time.time() - startTime)
-        print('Iterataion #{0} execution time: {1} (sec)'.format(ind, executionTime))
+        print('Iterataion #{0} execution time: {1} (sec) - phi sum: {2}'.
+              format(ind, round(executionTime, 2), np.abs(phi - theta).sum()))
     return theta
 
 if __name__ == '__main__':
     startTime = time.time()
+    # parameters
+    size = 5
+    p_slip = 0.0
     N=100
     batch_size=20
     meta_lr=0.1
-    theta=O.Constant(1.0)
-    theta = maml(N, batch_size, meta_lr, theta)
+    theta = maml(N, batch_size, meta_lr, size, p_slip)
     print('Theta: {0}'.format(theta))
     executionTime = (time.time() - startTime)
     print('Total execution time: {0} (sec)'.format(executionTime))
--- src/utils.py
+++ src/utils.py
@@ -1,62 +1,58 @@
 #!/usr/bin/env python
 
-import gridworld as W
-import maxent as M
-import plot as P
-import trajectory as T
-import solver as S
-import optimizer as O
+import gridworld as World
+import maxent as Maxent
+import plot as Plot
+import trajectory as Trajectory
+import solver as Solver
+import optimizer as Optimizer
 
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# CONSTANTS
-P_SLIP = 0
-SIZE = 5
-LOCATION = np.random.randint(1, SIZE**2)
-
-print(LOCATION)
-
-def setup_mdp():
+def setup_mdp(size=5, p_slip=0, location=None):
     """
     Set-up our MDP/GridWorld
     """
+    if location is None: location = np.random.randint(0, size**2)
     # create our world
-    world = W.IcyGridWorld(size=SIZE, p_slip=P_SLIP)
+    world = World.IcyGridWorld(size=size, p_slip=p_slip)
 
     # set up the reward function
     reward = np.zeros(world.n_states)
     # reward[-1] = 1.0
-    reward[LOCATION] = 1
+    reward[location] = 1
 
     # set up terminal states
-    # terminal = [SIZE**2 - 1]
-    terminal = [LOCATION]
+    terminal = [location]
 
     return world, reward, terminal
 
 
-def generate_trajectories(world, reward, terminal):
+def generate_trajectories(
+    world,
+    reward,
+    terminal,
+    n_trajectories = 200,
+    discount = 0.7,
+    weighting = lambda x: x**5
+):
     """
     Generate some "expert" trajectories.
     """
-    # parameters
-    n_trajectories = 200
-    discount = 0.7
-    weighting = lambda x: x**5
 
     # set up initial probabilities for trajectory generation
     initial = np.zeros(world.n_states)
     # initial[0] = 1.0
     initial = (1 / (world.n_states - 1)) * np.ones(world.n_states)
-    initial[LOCATION] = 0
+    initial[terminal[0]] = 0
 
     # generate trajectories
-    value = S.value_iteration(world.p_transition, reward, discount)
-    policy = S.stochastic_policy_from_value(world, value, w=weighting)
-    policy_exec = T.stochastic_policy_adapter(policy)
-    tjs = list(T.generate_trajectories(n_trajectories, world, policy_exec, initial, terminal))
+    value = Solver.value_iteration(world.p_transition, reward, discount)
+    policy = Solver.stochastic_policy_from_value(world, value, w=weighting)
+    policy_exec = Trajectory.stochastic_policy_adapter(policy)
+    tjs = list(Trajectory.generate_trajectories(n_trajectories, world, policy_exec, initial, terminal))
 
     return tjs, policy
 
@@ -66,18 +62,18 @@ def maxent(world, terminal, trajectories, theta=None):
     Maximum Entropy Inverse Reinforcement Learning
     """
     # set up features: we use one feature vector per state
-    features = W.state_features(world)
+    features = World.state_features(world)
 
     # choose our parameter initialization strategy:
     # initialize parameters with constant - this won't be used if theta is defined
-    init = O.Constant(1.0)
+    init = Optimizer.Constant(1.0)
 
     # choose our optimization strategy:
     # we select exponentiated gradient descent with linear learning-rate decay
-    optim = O.ExpSga(lr=O.linear_decay(lr0=0.2))
+    optim = Optimizer.ExpSga(lr=Optimizer.linear_decay(lr0=0.2))
 
     # actually do some inverse reinforcement learning
-    theta, reward = M.irl(world.p_transition, features, terminal, trajectories, optim, init, theta)
+    theta, reward = Maxent.irl(world.p_transition, features, terminal, trajectories, optim, init, theta)
 
     return theta, reward
 
@@ -87,17 +83,17 @@ def maxent_causal(world, terminal, trajectories, discount=0.7, theta=None):
     Maximum Causal Entropy Inverse Reinforcement Learning
     """
     # set up features: we use one feature vector per state
-    features = W.state_features(world)
+    features = World.state_features(world)
 
     # choose our parameter initialization strategy:
     # initialize parameters with constant - this won't be used if theta is defined
-    init = O.Constant(1.0)
+    init = Optimizer.Constant(1.0)
 
     # choose our optimization strategy:
     # we select exponentiated gradient descent with linear learning-rate decay
-    optim = O.ExpSga(lr=O.linear_decay(lr0=0.2))
+    optim = Optimizer.ExpSga(lr=Optimizer.linear_decay(lr0=0.2))
 
     # actually do some inverse reinforcement learning
-    theta, reward = M.irl_causal(world.p_transition, features, terminal, trajectories, optim, init, discount, theta)
+    theta, reward = Maxent.irl_causal(world.p_transition, features, terminal, trajectories, optim, init, discount, theta)
 
     return theta, reward
